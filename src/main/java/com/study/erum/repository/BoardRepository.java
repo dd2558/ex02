43d0bd869cc19e1cf9edcd81e39fdfa9500d9378
dd2558 <dd2558@naver.com>
2024-03-06 14:28:52 +0900

    메소드,쿼리,jsp파일추가

--- src/main/java/com/study/erum/repository/BoardRepository.java
+++ src/main/java/com/study/erum/repository/BoardRepository.java
@@ -1,8 +1,30 @@
 package com.study.erum.repository;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.study.erum.dto.BoardDTO;
+
+import lombok.RequiredArgsConstructor;
+
 @Repository
+@RequiredArgsConstructor
 public class BoardRepository {
+
+   private final SqlSessionTemplate sql;
+
+   public int save(BoardDTO boardDTO) {
+      return sql.insert("Board.insert",boardDTO);
+   }
+
+   public List<BoardDTO> findAll() {
+       return sql.selectList("Board.findAll");
+   }
+
+   public BoardDTO findById(Long id) {
+       return sql.selectOne("Board.findById", id);
+   }
 
 }
--- src/main/java/com/study/erum/service/BoardService.java
+++ src/main/java/com/study/erum/service/BoardService.java
@@ -1,7 +1,10 @@
 package com.study.erum.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.study.erum.dto.BoardDTO;
 import com.study.erum.repository.BoardRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -9,6 +12,19 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class BoardService {
-	
-	private final BoardRepository boardRepository;
+
+    private final BoardRepository boardRepository;
+
+    public int save(BoardDTO boardDTO) {
+        return boardRepository.save(boardDTO);
+    }
+
+    public List<BoardDTO> findAll() {
+        return boardRepository.findAll();
+    }
+
+    public BoardDTO findById(Long id) {
+        return boardRepository.findById(id);
+    }
+
 }
